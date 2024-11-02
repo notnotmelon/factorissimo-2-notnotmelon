e815c335106759b45be5d808d9435b2732f7de28
notnotmelon <zacharypicco@gmail.com>
2024-11-01 19:18:00 -0500

    feat: Growing crops indoors causes +50% extra spores.

--- script/pollution.lua
+++ script/pollution.lua
@@ -1,3 +1,8 @@
+local pollution_multipliers = {
+	spores = 1.5,
+	pollution = 1,
+}
+
 local function update_pollution(factory)
 	local inside_surface = factory.inside_surface
 	if not inside_surface.valid then return end
@@ -27,8 +32,13 @@ local function update_pollution(factory)
 
 	if pollution == 0 then return end
 
-	if factory.built then
-		factory.outside_surface.pollute({factory.outside_x, factory.outside_y}, pollution + factory.stored_pollution)
+	local outside_surface = factory.outside_surface
+	if factory.built and outside_surface.valid then
+		local pollutant_type = outside_surface.pollutant_type.name
+		local multiplier = pollution_multipliers[pollutant_type] or 1
+
+		local pollution_to_release = (pollution + factory.stored_pollution) * multiplier
+		outside_surface.pollute({factory.outside_x, factory.outside_y}, pollution_to_release)
 		factory.stored_pollution = 0
 	else
 		factory.stored_pollution = factory.stored_pollution + pollution
