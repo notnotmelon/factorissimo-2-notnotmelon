38c1691f0bf03d2f349bbabb4043c013d85a0435
Zachary Picco <zacharypicco@gmail.com>
2025-09-23 11:50:47 -0500

    fix: Fixed migration crash with factory eject chests. Resolves https://github.com/notnotmelon/factorissimo-2-notnotmelon/issues/233

--- script/roboport/roboport.lua
+++ script/roboport/roboport.lua
@@ -252,14 +252,14 @@ factorissimo.build_roboport_upgrade = function(factory)
 
     if factory.building and factory.building.valid then
         requester = requester or factory.outside_surface.create_entity {
-            name = factory.layout.outside_requester_chest,
+            name = factory.layout.outside_requester_chest or ("factory-requester-chest-" .. factory.building.name),
             position = factory.building.position,
             force = factory.force,
             quality = factory.quality,
         }
 
         ejector = ejector or factory.outside_surface.create_entity {
-            name = factory.layout.outside_ejector_chest,
+            name = factory.layout.outside_ejector_chest or ("factory-eject-chest-" .. factory.building.name),
             position = factory.building.position,
             force = factory.force,
             quality = factory.quality,
